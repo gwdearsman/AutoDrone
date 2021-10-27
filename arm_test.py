d8007dee7d9b1848ac875fbbd1d756b6df1d37dd
gwdearsman <gwdearsman@gmail.com>
2021-10-27 13:47:37 -0400

    Finished arming script

--- arm_test.py
+++ arm_test.py
@@ -8,5 +8,34 @@ import argparse
 
 
 def connectMyCopter():
-    parser
+    parser = argparse.ArgumentParser(description='commands')
+    parser.add_argument('--connect')
+    args = parser.parse_args()
+
+    connection_string = args.connect
+    baud_rate = 921600
+
+    vehicle = connect(connection_string, baud=baud_rate, wait_ready=True)
+    return vehicle
+
+def arm():
+    while vehicle.is_armable==False:
+        print("Waiting for vehicle to become armable...")
+        time.sleep(1)
+    print("Vehicle is armable")
+
+    vehicle.armed=True
+    while vehicle.armed==False:
+        print("Waiting for drone to arm")
+        time.sleep(1)
+
+    print("Vehicle is now armed.")
+    print("Props are now spinning")
+
+    return None
+
+
+vehicle = connectMyCopter()
+arm()
+print("end of script")
 
