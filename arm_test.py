2b95b7f5da792b51489413ac28c55efaae4d7482
gwdearsman <gwdearsman@gmail.com>
2021-10-27 13:37:57 -0400

    arm_test python script

---
+++ arm_test.py
@@ -0,0 +1,12 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+import time
+import socket
+import exceptions
+import math
+import argparse
+
+
+
+def connectMyCopter():
+    parser
+
