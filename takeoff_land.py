22204116b07927b06009bd5b1fb1ae0fb380485e
gwdearsman <gwdearsman@gmail.com>
2021-10-30 11:46:02 -0400

    added velocity mapping

--- takeoff_land.py
+++ takeoff_land.py
@@ -74,7 +74,32 @@ def land():
     print "returning to launch"
     vehicle.mode = VehicleMode("RTL")
 
+def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
+    """
+    Move vehicle in direction based on specified velocity vectors.
+    """
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, 0, 0, # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
 
+
+    # send command to vehicle on 1 Hz cycle
+    for x in range(0,duration):
+        vehicle.send_mavlink(msg)
+        time.sleep(1)
+# Set up velocity mappings
+# velocity_x > 0 => fly North
+# velocity_x < 0 => fly South
+# velocity_y > 0 => fly East
+# velocity_y < 0 => fly West
+# velocity_z < 0 => ascend
+# velocity_z > 0 => descend
 
 #####################################################################################################
 ########################################Start of Code################################################
@@ -90,6 +115,11 @@ vehicle.airspeed = 5
 arm()
 takeoff(20)
 move_to_pos(point1)
+send_ned_velocity(3,0,0,3)
+send_ned_velocity(0,3,0,3)
+send_ned_velocity(0,0,-3,3)
+send_ned_velocity(-3,-3,3,3)
+
 land()
 
 
