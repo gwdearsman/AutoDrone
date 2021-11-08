e4aceb4884c9982dc227a6148234560605fd2a44
gwdearsman <91651788+gwdearsman@users.noreply.github.com>
2021-11-08 14:58:53 -0500

    wrongly passing img instead of mask

--- guideLand.py
+++ guideLand.py
@@ -126,7 +126,7 @@ def findColor(img,White):
 def getContours(img,mask):
     #inputs a masked image from findColor and maps its contours
 	x,y,w,h = 320,220,0,0
-	contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+	contours,hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 	for cnt in contours:
 		area = cv2.contourArea(cnt)
 		if area>300:
