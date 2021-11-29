0f60fc2964511e2f6c0167d7fa192477ef8dc6e2
gwdearsman <91651788+gwdearsman@users.noreply.github.com>
2021-11-29 15:35:58 -0500

    Create codeSelector.py

---
+++ codeSelector.py
@@ -0,0 +1,7 @@
+import subprocess
+process = subprocess.Popen(["python3 outputTest.py"], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+#process.stdin.write("2 3")
+#process.stdin.close()
+print(process.stdout.read())
+process.wait()
+print("Completed!")
