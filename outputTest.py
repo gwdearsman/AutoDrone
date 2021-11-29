afcfab28a7f59e8773578882d936df0931f8b36f
gwdearsman <91651788+gwdearsman@users.noreply.github.com>
2021-11-29 17:16:22 -0500

    Update outputTest.py

--- outputTest.py
+++ outputTest.py
@@ -3,4 +3,6 @@ from time import *
 
 mylcd = I2C_LCD_driver.lcd()
 
-mylcd.lcd_display_string("Hello World!", 1)
+mylcd.lcd_display_string("Choose Mode", 1)
+mylcd.lcd_display_string(" 1   1   1   1  ", 2)
+
