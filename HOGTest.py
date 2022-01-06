53c347af90dc11de6633aa891a4f951f0f1f06c5
gwdearsman <91651788+gwdearsman@users.noreply.github.com>
2022-01-06 16:52:08 -0500

    reduced image resolution

--- HOGTest.py
+++ HOGTest.py
@@ -18,14 +18,14 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 cap = cv2.VideoCapture(0)
 
 #write output video
-#out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'MJPG'), 15., (640,480))
+out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'MJPG'), 15., (640,480))
 
 def findPerson():
     while(True):
         ret, img = cap.read()
 
         #resizing for smaller file
-        img = cv2.resize(img, (640, 480))
+        img = cv2.resize(img, (320, 240))
         imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         boxes, weights = hog.detectMultiScale(img, winStride=(8,8))
@@ -35,7 +35,7 @@ def findPerson():
             #display te detected boxes in img
             cv2.rectangle(img, (xA, yA), (xB, yB), (255,0,0), 2)
         
-        #out.write(img.astype('uint8'))
+        out.write(img.astype('uint8'))
         cv2.imshow('img', img)
         if cv2.waitKey(1) > 0:
             break
